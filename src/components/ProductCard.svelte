8c6cf527d8cc30dda9bec4e032c4de02eaffec13
analubarreto <analuibm@gmail.com>
2021-01-06 21:01:26 -0300

    finished basic card

--- src/components/ProductCard.svelte
+++ src/components/ProductCard.svelte
@@ -2,12 +2,13 @@
 	export let fruitName: String;
 	export let fruitPrice: Number;
 	export let unit: String;
+	export let imgUrl: any;
 </script>
 
 <div class="flex flex-col justify-center items-center bg-brown-light w-72 h-80 rounded-md">
+	<div class="mb-5"><img src={imgUrl} alt={fruitName} /></div>
 	<div>
 		<h4 class="text-brown-dark">{fruitName}</h4>
-
 		<p class="text-brown-dark">{fruitPrice}.00<small>{unit}</small></p>
 	</div>
 </div>
--- src/components/ProductGrid.svelte
+++ src/components/ProductGrid.svelte
@@ -7,54 +7,71 @@
 			fruitName: 'Avocado',
 			fruitPrice: 5,
 			unit: 'unit',
+			imageUrl: '',
+			isOrganic: true,
 		},
 		{
 			id: 'fruit2',
 			fruitName: 'Red Apple',
 			fruitPrice: 3,
 			unit: 'kg',
+			imageUrl: '',
+			isOrganic: false,
 		},
 		{
 			id: 'fruit3',
 			fruitName: 'Green Apple',
 			fruitPrice: 16,
 			unit: 'kg',
+			imageUrl: '',
+			isOrganic: true,
 		},
 		{
 			id: 'fruit4',
 			fruitName: 'Pineapple',
 			fruitPrice: 2,
 			unit: 'unit',
+			imageUrl: '',
+			isOrganic: false,
 		},
 		{
 			id: 'fruit5',
 			fruitName: 'Blue Berry',
 			fruitPrice: 12,
 			unit: 'kg',
+			isOrganic: true,
 		},
 		{
 			id: 'fruit6',
 			fruitName: 'Cherry',
 			fruitPrice: 10,
 			unit: 'box',
+			imageUrl: '',
+			isOrganic: true,
 		},
 		{
 			id: 'fruit7',
 			fruitName: 'Banana',
 			fruitPrice: 2,
 			unit: 'kg',
+			imageUrl: '',
+			isOrganic: false,
 		},
 		{
 			id: 'fruit8',
 			fruitName: 'Green Grapes',
 			fruitPrice: 7,
 			unit: 'box',
+			imageUrl: '',
+			isOrganic: false,
 		},
 		{
 			id: 'fruit9',
 			fruitName: 'Orange',
 			fruitPrice: 8,
 			unit: 'kg',
+			imageUrl: '',
+			isOrganic: true,
 		},
 	];
 </script>
@@ -64,6 +81,7 @@
 		<ProductCard
 			fruitName={fruit.fruitName}
 			fruitPrice={`R$ ${fruit.fruitPrice}`}
-			unit={fruit.unit} />
+			unit={fruit.unit}
+			imgUrl={fruit.imgUrl} />
 	{/each}
 </main>
